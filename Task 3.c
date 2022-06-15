6fb085564779bc866aacbca41fb2926947837230
Raquibul024 <73742694+Raquibul024@users.noreply.github.com>
2022-06-15 10:39:36 +0600

    Add files via upload

---
+++ Task 3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+    int arr[] = {10,20,30,40,50};
+    int max = arr[0];
+    int i, j=6, loc=1;
+    printf ("Given Array : \n");
+    for (i=0; i<j; i++)
+    {
+        printf("\t arr[%d] = %d\n", i, arr[i]);
+    }
+    for (i=0; i<j; i++)
+        {
+            if (arr[i]>max)
+            {
+                max = arr [i];
+                loc = i;
+            }
+        }
+        printf("\n Max value = %d, \t Location = %d", max, loc);
+        return 0;
+}
